064d1672f2f905e266969a79e0bc4dfb43989c65
nchantre <nelsonfabiancs@hotmail.com>
2021-11-29 21:37:36 -0500

    ciclos y validaciones
    29/11/2021 validacion del if

---
+++ Ejercicio/Ejercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresar numero de la tabla ");
+            int numero = Int32.Parse(Console.ReadLine());
+            int contador = 10;
+            for (int i = 1; i <= contador; i++)
+            {
+
+                Console.WriteLine( numero+"X"+i+"="+numero*i);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
